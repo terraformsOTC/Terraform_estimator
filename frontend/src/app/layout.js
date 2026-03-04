d63a9c4af977c3c94e05756d9e19b4b3e037e034
Vercel <vercel[bot]@users.noreply.github.com>
2026-03-04 22:58:21 +0000

    Install Vercel Speed Insights for Next.js
    This is an automated pull request generated by Vercel to help install [Speed Insights](https://vercel.com/docs/speed-insights) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata = {
   title: '[terraform estimator]',
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
-      <body>{children}</body>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
